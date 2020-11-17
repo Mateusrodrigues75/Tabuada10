ae7c9f2e63d73ee13b440756b84630dbc8387734
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-17 14:52:59 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, tabuada, multiplicacao;
+            Console.WriteLine("Digite um número Inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            for(tabuada = 1; tabuada<=10; tabuada++){
+                multiplicacao = num*tabuada;
+                Console.WriteLine($" {num} * {tabuada} = {multiplicacao}");
+            } 
+            
+        }
+    }
+}
